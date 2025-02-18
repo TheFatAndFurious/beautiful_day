6763c29a74bbb94c3d65b02e693c2db353b6d756
TheFatAndFurious <mrguerrilla@gmail.com>
2025-02-18 23:41:09 +0100

    Main logic for parsing arguments

--- APIQueries.py
+++ APIQueries.py
@@ -11,6 +11,9 @@ class APIQueries:
         }
 
     def current_weather(self, city_name):
+        """Fetch current weather data for a given city name.
+            :parameters: city_name: str
+        """
         url = f"https://open-weather13.p.rapidapi.com/city/{city_name}&units=metric/FR"
         headers = self.headers
         try:
@@ -21,11 +24,21 @@ class APIQueries:
         except requests.exceptions.RequestException as e:
             return {"error": str(e)}
 
+
+    #TODO: Create a dictionary so we can pass cities instead of lat and lon
     def forecast(self, lat:float, lon:float):
+        """Fetch 5-day weather forecast for a given location.
+            :parameters: lat: float, lon: float
+        """
         url = f"https://open-weather13.p.rapidapi.com/city/fivedaysforcast/{lat}/{lon}&units=metric"
         headers = self.headers
-        response = requests.get(url, headers=headers)
-        return response.json()
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            parsed_data = self._parse_forecast_response(response.json())
+            return parsed_data
+        except requests.exceptions.RequestException as e:
+            return {"error": str(e)}
 
     @staticmethod
     def _parse_current_response(response: dict):
@@ -34,7 +47,33 @@ class APIQueries:
             'temp_feels_like': response.get('main', {}).get('feels_like'),
         }
 
-def format_response(data):
+    @staticmethod
+    def _parse_forecast_response(response: dict):
+        data = response.get('list', {})
+        weather_array = []
+        for occurence in data:
+            weather_array.append({
+                'date': occurence.get('dt_txt'),
+                'temperature': occurence.get('main', {}).get('temp'),
+                'temp_feels_like': occurence.get('main', {}).get('feels_like'),
+                'weather': occurence.get('weather', [{}])[0].get('description')
+            })
+        return weather_array
+def format_forecast_response(data):
+    console = Console()
+    table = Table(show_header=True, header_style="bold magenta")
+
+    table.add_column("Date")
+    table.add_column("Temperature")
+    table.add_column("Feels like")
+    table.add_column("Weather")
+
+    for occurence in data:
+        table.add_row(occurence.get('date'), str(occurence.get('temperature')), str(occurence.get('temp_feels_like')), occurence.get('weather'))
+
+    console.print(table)
+
+def format_current_response(data):
     console = Console()
     table = Table(show_header=True, header_style="bold magenta")
 
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import typer
 from rich.console import Console
 
-from APIQueries import APIQueries, format_response
+from APIQueries import APIQueries, format_current_response, format_forecast_response
 
 
-def main(name: str = ""):
+def main(choice: str, name: str ):
     console = Console()
     qery = APIQueries(api_key="2a3f462634msh63a4a8280da611ep1352acjsn54d03d9ef887")
-    #with console.status("[bold green]Fetching data...[/bold green]", spinner="dots"):
-    #    response = qery.current_weather(name)
-    #format_response(response)
-    print(qery.forecast(48.8583, 2.2945))
-
-
+    if choice == "cur":
+        with console.status("[bold green]Fetching data...[/bold green]", spinner="dots"):
+            response = qery.current_weather(name)
+        format_current_response(response)
+    elif choice == "for":
+        with console.status("[bold green]Fetching data...[/bold green]", spinner="dots"):
+            response = qery.forecast(48.8583, 2.2945)
+            format_forecast_response(response)
 if __name__ == "__main__":
     typer.run(main)
